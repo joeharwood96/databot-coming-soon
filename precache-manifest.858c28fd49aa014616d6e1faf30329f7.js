9326f256ba0804fe2bc394cbf8251f4e138c5bd4
Sandy Kane <sakane@Sandys-MacBook-Pro.local>
2020-10-06 18:35:49 +0100

    Updates

---
+++ precache-manifest.858c28fd49aa014616d6e1faf30329f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e10e3706fe06e53bb78a24cce9dd1cd",
+    "url": "/databot-coming-soon/index.html"
+  },
+  {
+    "revision": "a83a19b941e963f7a48c",
+    "url": "/databot-coming-soon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "133a52512a4e95e69597",
+    "url": "/databot-coming-soon/static/js/2.277f06c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/databot-coming-soon/static/js/2.277f06c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83a19b941e963f7a48c",
+    "url": "/databot-coming-soon/static/js/main.fdb95cef.chunk.js"
+  },
+  {
+    "revision": "66a8bae3bdba761cdeb9",
+    "url": "/databot-coming-soon/static/js/runtime-main.c38dfe18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/databot-coming-soon/static/media/logo.5d5d9eef.svg"
+  }
+]);
